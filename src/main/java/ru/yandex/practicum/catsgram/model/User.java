68dbafee6ceca6356ebb53f02a6a14e022d01123
Zubarev Dmitry <dmitryshikary@yandex.ru>
2024-09-28 09:52:40 +0300

    main branch

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.catsgram.model;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "email")
+public class User {
+    Long id;
+    String username;
+    String email;
+    String password;
+}
